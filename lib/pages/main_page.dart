8cdaefdffaf25f22b345d129e7249d20ae9e71ae
Ahmed Gamal <ahmedmgh67@gmail.com>
2019-05-13 01:45:52 +0200

    Update main_page.dart

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,7 +1,7 @@
 import 'package:emania/import.dart';
 
-
 MyAppState appState;
+
 class MyApp extends StatefulWidget {
   static ShoppingBasket shoppingBasket = ShoppingBasket();
   static WishList wishList = WishList();
@@ -20,7 +20,6 @@ class MyAppState extends State<MyApp> {
 
   List<Product> pL;
 
-
   /*List<Product> shuffleAndReturn(List<Product> products) {
     List<Product> r = products;
     r.shuffle();
@@ -33,7 +32,6 @@ class MyAppState extends State<MyApp> {
     request();
     //Timer(Duration(seconds: 3),() => setState(() {isLoaded = true;}) );
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -51,30 +49,34 @@ class MyAppState extends State<MyApp> {
                 style: TextStyle(fontSize: 28.0),
               ),
             ),
-          ]..addAll(Data.categories.map((c) => Container(
-                child: InkWell(
-                  onTap: () {
-                    Navigator.of(context).push(
-                        MaterialPageRoute(builder: (ctx) => CategoryPage(c)));
-                  },
-                  child: Container(
-                    margin: EdgeInsets.only(bottom: 16.0),
-                    child: Row(
-                      mainAxisSize: MainAxisSize.max,
-                      children: <Widget>[
-                        Padding(
-                          padding: const EdgeInsets.only(left: 8.0),
-                          child: Icon(c.icon),
+          ]..addAll(
+              Data.categories.map(
+                (c) => Container(
+                      child: InkWell(
+                        onTap: () {
+                          Navigator.of(context).push(MaterialPageRoute(
+                              builder: (ctx) => CategoryPage(c)));
+                        },
+                        child: Container(
+                          margin: EdgeInsets.only(bottom: 16.0),
+                          child: Row(
+                            mainAxisSize: MainAxisSize.max,
+                            children: <Widget>[
+                              Padding(
+                                padding: const EdgeInsets.only(left: 8.0),
+                                child: Icon(c.icon),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(left: 8.0),
+                                child: Text(c.name),
+                              ),
+                            ],
+                          ),
                         ),
-                        Padding(
-                          padding: const EdgeInsets.only(left: 8.0),
-                          child: Text(c.name),
-                        ),
-                      ],
+                      ),
                     ),
-                  ),
-                ),
-              ))),
+              ),
+            ),
         ),
       ),
       key: scaffoldKey,
@@ -83,72 +85,72 @@ class MyAppState extends State<MyApp> {
           searchTerm = s;
         });
       }, scaffoldKey),
-      body: pL == null ? Center(child: CircularProgressIndicator()) : searchTerm == ""
-          ? ListView(
-              physics: ClampingScrollPhysics(),
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.only(left: 16.0, top: 16.0),
-                  child: Text(
-                    "Category",
-                    style: TextStyle(
-                        fontWeight: FontWeight.w100, color: Color(0xff444444)),
-                  ),
-                ),
-                CategoryCardScroller(),
-                Padding(
-                  padding: const EdgeInsets.only(left: 16.0),
-                  child: Text(
-                    "Hot Items",
-                    style: TextStyle(
-                        fontWeight: FontWeight.w100, color: Color(0xff444444)),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: GridView.count(
-                    physics: NeverScrollableScrollPhysics(),
-                    crossAxisCount: 2,
-                    childAspectRatio: 0.798,
-                    mainAxisSpacing: 16.0,
-                    crossAxisSpacing: 16.0,
-                    /*children: Data.products
+      body: pL == null
+          ? Center(child: CircularProgressIndicator())
+          : searchTerm == ""
+              ? ListView(
+                  physics: ClampingScrollPhysics(),
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.only(left: 16.0, top: 16.0),
+                      child: Text(
+                        "Category",
+                        style: TextStyle(
+                            fontWeight: FontWeight.w100,
+                            color: Color(0xff444444)),
+                      ),
+                    ),
+                    CategoryCardScroller(),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 16.0),
+                      child: Text(
+                        "Hot Items",
+                        style: TextStyle(
+                            fontWeight: FontWeight.w100,
+                            color: Color(0xff444444)),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: GridView.count(
+                        physics: NeverScrollableScrollPhysics(),
+                        crossAxisCount: 2,
+                        childAspectRatio: 0.798,
+                        mainAxisSpacing: 16.0,
+                        crossAxisSpacing: 16.0,
+                        /*children: Data.products
                         //.where((p) => p.id < 4)
                         .map((p) => ProductCard(p))
                         .toList(),*/
-                    children: 
-                      pL
-                      .where((p) => p.id < 10)
-                      .map((p) => ProductCard(p))
-                      .toList()
-                    ,
-                    shrinkWrap: true,
-                  ),
-                )
-              ],
-            )
-          : Container(
-              child: ListView.builder(
-                itemCount: pL
-                    .where(
-                      (p) => p.name.toLowerCase().contains(
-                            searchTerm.toLowerCase(),
-                          ),
+                        children: pL
+                            .where((p) => p.id < 10)
+                            .map((p) => ProductCard(p))
+                            .toList(),
+                        shrinkWrap: true,
+                      ),
                     )
-                    .length,
-                itemBuilder: (ctx, index) {
-                  return ProductListItem(
-                    getProducts()
+                  ],
+                )
+              : Container(
+                  child: ListView.builder(
+                    itemCount: pL
+                              .where(
+                                (p) => p.name.toLowerCase().contains(
+                                      searchTerm.toLowerCase(),
+                                    ),
+                              ).length,
+                    itemBuilder: (ctx, index) {
+                      return ProductListItem(
+                        pL
                         .where(
                           (p) => p.name.toLowerCase().contains(
                                 searchTerm.toLowerCase(),
                               ),
-                        )
-                        .toList()[index],
-                  );
-                },
-              ),
-            ),
+                        ).toList()[index],
+                      );
+                    },
+                  ),
+                ),
     );
   }
 }
